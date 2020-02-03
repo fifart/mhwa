8832500dd419c34496e2cfc2456978e180799524
fifart <fifart@gmail.com>
2020-02-03 16:05:46 +0200

    Show first Model

---
+++ collection/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from collection.models import Thing
+
+# Create your views here.
+def index(request):
+    things = Thing.objects.all()
+    return render(request, 'index.html', {
+        'things': things,
+    })
+
+def thing_detail(request, slug):
+    thing = Thing.objects.get(slug=slug)
+    return render(request, 'things/thing_detail.html', {
+        'thing':thing,
+    })
